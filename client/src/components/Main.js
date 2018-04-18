d04d56ea9c8a1bae0bc4f673052c76f579ffc095
artkwi <artkwi@st.amu.edu.pl>
2018-04-18 20:36:21 +0200

    Rejestracja - komponent

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,12 +1,14 @@
 // To jest Main - środkowa część strony wyświetlająca część główną
 import React from 'react';
-import Cel from './Cel'
+import Cel from './Cel';
+import Rejestracja from './Rejestracja';
 
 class Main extends React.Component {
     render() {
         return (
             <div>
                 Main
+                <Rejestracja />
 
             </div>
         )
