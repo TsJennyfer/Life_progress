1bf0e6133fa1ef689192791ddf69436ae494b632
artkwi <artkwi@st.amu.edu.pl>
2018-05-15 17:08:56 +0200

    Niedziałające przesyłanie tokena

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next)=>{
     try {
-        const token = req.headers.authorization.split(" ")[1];
+        const token = req.body.headers.authorization.split(" ")[1];
+        //const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3RAdGVzdC5wbCIsInVzZXJJZCI6IjVhZjJkOWJmNmQyZDQ1MDZlODBmNmFkMiIsImlhdCI6MTUyNjM5NTUyNCwiZXhwIjoxNTI2Mzk5MTI0fQ.qqWCQjRSMhanGeWIYhPSGKpLKVclkJuShyxGJPw9lqw";
         console.log(token);
         const decoded = jwt.verify(token, 'secret');
         req.userData = decoded;
