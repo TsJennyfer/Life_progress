6c8994a009ad82f5c698a541c43187f01d18ac93
sebkot <sebkot@st.amu.edu.pl>
2018-10-24 20:18:35 +0200

    gitignore

---
+++ db/email.js
@@ -0,0 +1,7 @@
+var Email = {
+    emailAdress: "lifeprogress.pri@gmail.com",
+    emailPassword: process.env.EMAIL_PASSWORD,
+    emailMessage: "Hello in our App !"
+};
+
+module.exports = {Email};
