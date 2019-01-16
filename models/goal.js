43b7707e1cd4996eb8ac55873c5ef554fdf54783
sebkot <sebkot@st.amu.edu.pl>
2019-01-16 11:22:10 +0100

    backend frontend signs limit

--- models/goal.js
+++ models/goal.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 
 var Goal = mongoose.model('Goal', {
-    name: {type: String, required: true, minlenght: 3, trim: true},
-    description: {type: String, default: null},
+    name: {type: String, required: true, minlength: 3, maxlength: 100, trim: true},
+    description: {type: String, maxlength: 1000, default: null},
     category: {type: String, default: null},
     completed: {type: Boolean, default: false},
     createdAt: {type: Number, default: null},
